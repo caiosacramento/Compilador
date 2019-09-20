281cdcd38e500e55d34a1c05d74b8c5401eeef4c
Leonardo Araujo <leocrazy007@hotmail.com>
2019-09-20 14:56:44 -0300

    add program with switch for specific case

--- atividadeTesteSlide.c
+++ atividadeTesteSlide.c
@@ -7,35 +7,35 @@
 //funcao que realiza a analise
 bool  analise(char  palavra[])  { 
     int k  =  0; 
-    while  (palavra[k]  ==  '$'  ||   palavra[k]  ==  '+'  ||  palavra[k]  ==  '-'  ){ 
+    while  (palavra[k]  ==  '$'  ||   palavra[k]  ==  '+'  ||  palavra[k]  ==  '-'  )
         printf("%c\n",palavra[k]);
         k++;
         
-    if  ( palavra[k]  ==  '0'  ||  palavra[k]  ==  '1'  ) {
-        printf("%c\n",palavra[k]);
+    if  ( palavra[k]  ==  '0'  ||  palavra[k]  ==  '1'  ) 
+        //printf("%c\n",palavra[k]);
         k++; 
-        }
-    else         
-        return  FALSE; 
-
+        
+    //else         
+      //  return  FALSE; 
     
-    if  (palavra[k]  ==  '#'  ) { 
-        printf("%c\n",palavra[k]); 
+    if  (palavra[k]  ==  '#'  )  
+        //printf("%c\n",palavra[k]); 
         k++; 
-        if  (palavra[k]  >=  'a'   &&   palavra[k]  <=  'z' )  { 
-            printf("%c\n",palavra[k]);
+
+        if  (palavra[k]  >=  'a'   &&   palavra[k]  <=  'z' )   {
+            //printf("%c\n",palavra[k]);
             k++; 
-            return  TRUE; 
-        } 
-        else 
-            return  FALSE; 
-    } 
- }
+            return  TRUE; }
+         
+    else 
+            return  FALSE;
+    
 }
+
 //funcao main adicionada para executar funcao do slide
 int main() { 
 
-    char palavra[]="1111\n";
+    char palavra[]="eee\n";
     printf("Palavra: %s",palavra);
     printf("retorno: %d",analise(palavra));
     return 0;
