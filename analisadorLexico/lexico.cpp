f134e08d6fb2225e22f041a3615a692a69ba5edd
Leonardo Araujo <leocrazy007@hotmail.com>
2019-11-02 16:42:00 -0300

    fix bug line and colums and add struct token

--- analisadorLexico/lexico.cpp
+++ analisadorLexico/lexico.cpp
@@ -11,13 +11,14 @@ using namespace std;
 int linha=1, coluna=1,q=0,auxPos=0;
 
 //struct guarda o lexema e suas informacoes
-struct lexema{
+struct Lexema{
 	int linha;
 	int coluna;
 	string nomeToken;
 	string valorToken;
 };
 
+vector<Lexema>listaTokens;
 
 string palavrasReservadas[41]={	"programainicio",
 								"execucaoinicio",
@@ -65,7 +66,7 @@ string palavrasReservadas[41]={	"programainicio",
 struct token{
 	string nome="";
 	string conteudo="";
-	};
+};
 
 void atualizaLinha(){
 	q++;
@@ -84,12 +85,15 @@ void tratarOtherSymbols(char leitura[]){
 		if((leitura[q]=='\n')){
 			linha++;
 			coluna=1;
-			break;}
+			break;
+			}
 		if(leitura[q]==' '){
 			break;
 		}
 		q++;
 		coluna++;
+		//adicionado break
+		break;
 	}
 }
 
@@ -116,13 +120,15 @@ void tratarReservadas(char leitura[Tam_Maximo]){
 	bool flag1=false;
 	string token="";
 	string tokenLower="";
+	Lexema auxToken; 
 	int i; 
 	while(1){ 
-		if(leitura[q]=='\n'){
+		if(leitura[q]==10){
 			linha++;
 			coluna=1;
+			break;
 		}
-		if((leitura[q]==' ')||(leitura[q]=='\n')){
+		if((leitura[q]==' ')){
 			break;}
 
 		if(((leitura[q]<48) || (leitura[q]>57))  && ((leitura[q]<65) || (leitura[q]>90)) && ((leitura[q]<97) || (leitura[q]>122))){
@@ -134,6 +140,8 @@ void tratarReservadas(char leitura[Tam_Maximo]){
 		}
 
 		token+=leitura[q];	
+		auxToken.linha=linha;
+		auxToken.coluna=coluna;
 		q++;
 		coluna++;
 	}
@@ -147,9 +155,17 @@ void tratarReservadas(char leitura[Tam_Maximo]){
 	}
 	if(!flag1){
 		if (flag){
-			cout << token << " -- Keyword" << endl;}
+			cout << token << " -- Keyword --" << auxToken.linha << " Coluna " << auxToken.coluna << endl;
+			auxToken.nomeToken="Keyword";
+			auxToken.valorToken=token;
+			listaTokens.push_back(auxToken);
+			}
 		else {
-			cout << token << " -- Identify" << endl;}
+			cout << token << " -- Identify" << auxToken.linha << " Coluna " << auxToken.coluna << endl;
+			auxToken.nomeToken="Identify";
+			auxToken.valorToken=token;
+			listaTokens.push_back(auxToken);
+			}
 	}
 }
 
@@ -157,6 +173,7 @@ void tratarReservadas(char leitura[Tam_Maximo]){
 void tratarNumeros(char leitura[Tam_Maximo]){
 	bool flag=false;
 	string token="";
+	Lexema auxToken; 
 
 	while(1){ 
 		if(leitura[q]=='\n'){
@@ -176,11 +193,16 @@ void tratarNumeros(char leitura[Tam_Maximo]){
 			break;
 		}
 		token+=leitura[q];	
+		auxToken.linha=linha;
+		auxToken.coluna=coluna;
 		q++;
 		coluna++;
 	}
 	if(!flag){
-		cout<<token<< " -- Number" << endl;	
+		cout<<token<< " -- Number" << auxToken.linha << "Coluna" << auxToken.coluna <<endl;	
+		auxToken.nomeToken="Number";
+		auxToken.valorToken=token;
+		listaTokens.push_back(auxToken);
 	}
 }
 
@@ -224,9 +246,14 @@ while(q<tamanhoLeitura){
 			else{
 				cout<<"Erro Encontrado no Caractere Desconhecido: " << lido << " Na Linha "<< linha << " Coluna mais um " << coluna+1 << endl;
 				tratarOtherSymbols(leitura);
-			}
-			
+			}		
 		}
 	q++;
 }
+
+cout << endl << endl;
+for(int i=0; i<listaTokens.size(); i++){
+		cout<<"\n"<<listaTokens[i].nomeToken<<" "<<listaTokens[i].valorToken<<"\n";
+		cout<<listaTokens[i].linha<<" "<<listaTokens[i].coluna<<"\n";
+  }  
 }
